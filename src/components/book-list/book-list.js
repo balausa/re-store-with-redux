8d53c58c95ac0c443ae6b255371cc08013e13cb1
balausa <52880784+balausa@users.noreply.github.com>
2022-12-07 17:52:37 +0600

    added thunkMiddleware

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import BookListItem from '../book-list-item';
-
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-
 import { withBookstoreService } from '../hoc';
 import { fetchBooks, bookAddedToCart } from '../../actions';
 import compose from '../../utils';
@@ -57,10 +56,14 @@ const mapStateToProps = ({ bookList: { books, loading, error }}) => {
 
 const mapDispatchToProps = (dispatch, { bookstoreService }) => {
 
-  return {
-    fetchBooks: fetchBooks(bookstoreService, dispatch),
-    onAddedToCart: (id) => dispatch(bookAddedToCart(id))
-  };
+//   return {
+//     fetchBooks: fetchBooks(bookstoreService, dispatch),
+//     onAddedToCart: (id) => dispatch(bookAddedToCart(id))
+//   };
+return bindActionCreators({
+    fetchBooks: fetchBooks(bookstoreService),
+    onAddedToCart: bookAddedToCart
+  }, dispatch);
 };
 
 export default compose(
--- src/store.js
+++ src/store.js
@@ -1,6 +1,34 @@
-import {createStore} from 'redux'
-import reducer from './reducers'
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 
-const store = createStore(reducer);
+import reducer from './reducers';
+
+
+const logMiddleware = ({ getState }) => (next) => (action) => {
+  console.log(action.type, getState());
+  return next(action);
+};
+
+const stringMiddleware = () => (next) => (action) => {
+  if (typeof action === 'string') {
+    return next({
+      type: action
+    });
+  }
+
+  return next(action);
+};
+
+const store = createStore(reducer, applyMiddleware(
+  thunkMiddleware, stringMiddleware, logMiddleware));
+
+
+const delayedActionCreator = (timeout) => (dispatch) => {
+  setTimeout(() => dispatch({
+    type: 'DELAYED_ACTION'
+  }), timeout);
+};
+
+store.dispatch(delayedActionCreator(3000));
 
 export default store;
